f73192ff38cf17231e2d8517a0ad865ca09563ea
LEROY Dorian <dorian.leroy@irisa.fr>
2021-04-21 09:35:09 +0200

    prepare release 0.4.1

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ echo "Building truffle interpreter component."
 cd $1
 mvn package
 cd $start/truffle
-mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla/target/fr.cea.nabla-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
-mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla.ir/target/fr.cea.nabla.ir-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.ir    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
-mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla.javalib/target/fr.cea.nabla.javalib-0.4.0-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.javalib    -Dversion=0.4.0-SNAPSHOT    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla/target/fr.cea.nabla-0.4.1-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla    -Dversion=0.4.1    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla.ir/target/fr.cea.nabla.ir-0.4.1-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.ir    -Dversion=0.4.1    -Dpackaging=jar    -DgeneratePom=true
+mvn install:install-file    -Dfile=$1/plugins/fr.cea.nabla.javalib/target/fr.cea.nabla.javalib-0.4.1-SNAPSHOT.jar    -DgroupId=fr.cea.nabla    -DartifactId=fr.cea.nabla.javalib    -Dversion=0.4.1    -Dpackaging=jar    -DgeneratePom=true
 mvn package
 echo "Done."
 
